18c9df3f59e3a84287fcaad09c052879b9174ad9
David J. Zielinski <djzielin@gmail.com>
2020-05-15 01:23:43 -0400

    more graceful exiting

--- client.js
+++ client.js
@@ -1,14 +1,16 @@
 const WebSocket = require('ws');
 const midi = require('midi');
-//var prompt = require('prompt');
 const readline = require('readline');
 
 const midiInput = new midi.Input();
+
+
 
 const rl = readline.createInterface({
 	input: process.stdin,
 	output: process.stdout
   });
+
 console.clear();
 
 console.log("midi ports available: ");
@@ -16,14 +18,17 @@ for (let i = 0; i < midiInput.getPortCount(); i++) {
 	console.log("["+ i + "] " + midiInput.getPortName(i));
 }
 
-
-//prompt.get(schema, function (err, result) {
 rl.question('What port would you like to use? ', (answer) => {
 	const ws = new WebSocket('ws://45.55.43.77:3902');  
 	ws.on('error', error => {
 		console.log("ERROR: couldn't connect to remote server.");
-		console.log(error);
-		process.exit();
+		//console.log(error);
+		niceExit();
+	});
+
+	ws.on('close',(code, reason) => {
+		console.log("Connection Closed: " + reason);
+		niceExit();
 	});
 
 	midiInput.openPort(parseInt(answer));
@@ -45,9 +50,25 @@ rl.question('What port would you like to use? ', (answer) => {
 	});
 });
 
+function niceExit() {
+	try{
+		midiInput.closePort();
+	}catch(err)
+	{
+		//console.log("couldn't close midi port");
+	}
+	
+	try{
+		ws.terminate();
+	}catch(err)
+	{
+		//console.log("couldn't close ws connection (perhaps not setup yet)");
+	}
+	process.exit();
+}
+
 process.on("SIGINT", function () {
-  console.log("received SIGINT (control-c). shutting down gracefully");
-  input.closePort();
-  ws.terminate();
-  process.exit();
+	console.log("received SIGINT (control-c). shutting down gracefully");
+	niceExit();
 });
+

db51419430367ca0376a15a313f4d27180766997
David J. Zielinski <djzielin@gmail.com>
2020-05-18 00:46:00 -0400

    try to keep reconnecting every second on connection loss

--- client.js
+++ client.js
@@ -3,7 +3,7 @@ const midi = require('midi');
 const readline = require('readline');
 
 const midiInput = new midi.Input();
-
+let ws = null;
 
 
 const rl = readline.createInterface({
@@ -19,18 +19,6 @@ for (let i = 0; i < midiInput.getPortCount(); i++) {
 }
 
 rl.question('What port would you like to use? ', (answer) => {
-	const ws = new WebSocket('ws://45.55.43.77:3902');  
-	ws.on('error', error => {
-		console.log("ERROR: couldn't connect to remote server.");
-		//console.log(error);
-		niceExit();
-	});
-
-	ws.on('close',(code, reason) => {
-		console.log("Connection Closed: " + reason);
-		niceExit();
-	});
-
 	midiInput.openPort(parseInt(answer));
 	midiInput.ignoreTypes(true, true, true); 	// Order: (Sysex, Timing, Active Sensing)
 	midiInput.on('message', (deltaTime, message) => {
@@ -40,17 +28,49 @@ rl.question('What port would you like to use? ', (answer) => {
 
 		if ((command & 0xF0) === 0x90) { // note on message
 			console.log(`NoteOn: ${message}`);
-			ws.send("[" + note + "," + vel + "]");
+			if(ws){
+				ws.send("[" + note + "," + vel + "]");
+			}
 		}
 		if ((command & 0xF0) === 0x80) { // note off message
 			console.log(`NoteOff: ${message}`);
 			vel = 0;
-			ws.send("[" + note + "," + vel + "]");
+			if(ws){
+				ws.send("[" + note + "," + vel + "]");
+			}
 		}
 	});
+
+	setInterval(() => {
+		if (ws === null) {
+			console.log("------------------------------------------------------------");
+			console.log("1 seconds has expired. trying to connect to server... ");
+			connectToServer();
+		}
+	}, 1000);
 });
 
-function niceExit() {
+function connectToServer() {
+	ws = new WebSocket('ws://45.55.43.77:3902');
+
+	ws.on('error', (error) => {
+		console.log("ERROR: couldn't connect to remote server.");
+		ws = null;
+	});
+
+	ws.on('close', (code, reason) => {
+		console.log("CLOSE: connection closed");
+		ws = null;
+	});
+
+	ws.on('open', ()=>{ console.log("Success! we are connect to server!"); });
+}
+
+
+
+process.on("SIGINT", () =>{
+	console.log("received SIGINT (control-c). shutting down gracefully");
+
 	try{
 		midiInput.closePort();
 	}catch(err)
@@ -65,10 +85,4 @@ function niceExit() {
 		//console.log("couldn't close ws connection (perhaps not setup yet)");
 	}
 	process.exit();
-}
-
-process.on("SIGINT", function () {
-	console.log("received SIGINT (control-c). shutting down gracefully");
-	niceExit();
 });
-
